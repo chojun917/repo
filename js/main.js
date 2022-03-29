27661aa477564ab3713ebb52a0f3061790126759
chojun917 <96730816+chojun917@users.noreply.github.com>
2022-03-29 23:53:30 +0900

    끔찍한버그발생 Cannot read property 'getElementsByTagName' of null

--- js/main.js
+++ js/main.js
@@ -18,3 +18,40 @@ function moveBackToTop() {
 
 window.addEventListener("scroll", checkScroll);
 backToTop.addEventListener("click", moveBackToTop);
+
+function transformPrev(event) {
+  const slidePrev = event.target;
+  const slideNext = slidePrev.nextElementSibling;
+
+  const classList = slidePrev.parentElement.nextElementSibling;
+  let activeLi = classList.getAttribute("data-position");
+  const liList = classList.getElementsByTagName("li");
+
+  if (classList.clientWidth < liList.length * 260 + Number(activeLi)) {
+    activeLi = Number(activeLi) - 260;
+
+    slideNext.style.color = "red";
+    slideNext.classList.add("slide-next-hover");
+  }
+
+  classList.style.transition = "transform 1s";
+  classList.style.transform = "translateX(" + String(activeLi) + "px)";
+  classList.setAttribute("data-position", activeLi);
+}
+
+const slidePrevList = document.getElementsByClassName("slide-prev");
+
+for (let i = 0; i < slidePrevList.length; i++) {
+  let classList =
+    slidePrevList[i].parentElement.parentElement.nextElementSibling;
+  let liList = classList.getElementsByTagName("li");
+
+  if (classList.clientWidth < liList.length * 260) {
+    slidePrevList[i].classList.add("slide-prev-hover");
+    slidePrevList[i].addEventListener("click", transformPrev);
+  } else {
+    const arrowContainer = slidePrevList[i].parentElement;
+    arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+    arrowContainer.removeChild(slidePrevList[i]);
+  }
+}
